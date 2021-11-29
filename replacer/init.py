ade17124b5f5152c796ef7bccd77872936b304a9
WNick <wnick@bk.ru>
2021-11-29 22:06:14 +0300

    hw_5

--- replacer/init.py
+++ replacer/init.py
@@ -25,7 +25,7 @@ def init_stage_tables():
         stage_connector.commit()
     print("schema inited")
 
-def get_data(tname):
+def get_data(tname, start_row = 0):
     with prod_connector.cursor() as cursor:
         fpath = os.path.join("/dumps","{}.csv".format(tname))
         try:
@@ -33,7 +33,8 @@ def get_data(tname):
         except:
             pass
         with open(fpath, "w+") as ifile:
-            cursor.copy_expert('''COPY (SELECT * FROM {}) TO STDOUT WITH (FORMAT CSV)'''.format(tname), ifile)
+            cursor.copy_expert('''COPY (SELECT * FROM {} WHERE {} > {}) TO STDOUT WITH (FORMAT CSV)'''
+            .format(tname,tname[:-1]+'_id',start_row), ifile)
     print("dumped", tname)
 
 def put_data(tname):
@@ -44,10 +45,17 @@ def put_data(tname):
         stage_connector.commit()
     print("uploaded", tname)
 
+def get_maxID(tname):
+    with stage_connector.cursor() as cursor:
+        cursor.execute("SELECT max({}) from {}".format(tname[:-1]+'_id',tname))
+        record = cursor.fetchall()
+    print("Результат", record[0][0])
+    return record[0][0]
+
 if __name__ == "__main__":
     init_stage_tables()
     for tname in ["spellers", "spells"]:
-        get_data(tname)
+        get_data(tname,get_maxID(tname))
         put_data(tname)
     while True:
         time.sleep(20)
